6d241f5b2a66e027bbd6fa03a776f78ab4222b40
krs8dd <krs8dd@umsystem.edu>
2024-02-06 16:21:19 -0600

    Updated user file to loop, next is forking it

--- user.c
+++ user.c
@@ -1,15 +1,21 @@
 #include<unistd.h>
 #include<sys/types.h>
+
 #include<stdio.h>
 #include<stdlib.h>
+
 int main(int argc, char** argv) {
-    printf("Hello from user.c, a new executable!\n");
-    printf("My process id is: %d\n",getpid());
-    printf(" I got %d arguments: \n", argc);
-    int i;
-    for (i =0; i < argc; i++)
-        printf("|%s| ", argv[i]);
-    printf("\nUser is now ending.\n");
-    sleep(3);
-    return EXIT_SUCCESS;
+
+    int iter = atoi(argv[1]);
+
+    pid_t cPid = getpid();
+    pid_t pPid = getppid();
+
+    for (int i = 0; i <= iter; ++i) {
+        printf("USER PID:%d PPID:%d Iteration:%d before sleeping\n" , cPid, pPid, i);
+        sleep(1);
+        printf("USER PID:%d PPID:%d Iteration:%d after sleeping\n" , cPid, pPid, i);
+    }
+
+    return (EXIT_SUCCESS);
 }
